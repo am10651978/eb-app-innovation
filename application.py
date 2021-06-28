30019434e9d3e72459ff0e7a90a75c535cc52ede
am10651978 <82416150+am10651978@users.noreply.github.com>
2021-06-28 11:12:31 -0500

    Update -10

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ welcome = """
 <body id="sample">
   <div class="textColumn">
     <h1>Stifel Innoation</h1>
-    <p> This is new contents </P>
+    <p> This is new contents.... Changess </P>
        <p>Elastic Beanstalk using Python....<br> This is great !!!</p>
  
   </div>
